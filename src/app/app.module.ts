063cc8ec453121dade4ea4446d41f2624e1503c1
Juan Ignacio Carrozzo <Lobezno@Lobocop.sisorg.com.ar>
2018-08-15 16:39:57 -0300

    firestore config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,32 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+const enviroment = {
+	apiKey: 'AIzaSyARfgLkXpEIkkoqU_NnWZLAAwDLQZAJm0U',
+	authDomain: 'market-list-9fbc7.firebaseapp.com',
+	databaseURL: 'https://market-list-9fbc7.firebaseio.com',
+	projectId: 'market-list-9fbc7',
+	storageBucket: 'market-list-9fbc7.appspot.com',
+	messagingSenderId: '703461982313'
+};
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+	declarations: [ MyApp ],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(MyApp),
+		AngularFireModule.initializeApp(enviroment),
+		AngularFireAuthModule,
+		AngularFirestoreModule
+	],
+	bootstrap: [ IonicApp ],
+	entryComponents: [ MyApp ],
+	providers: [ StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler } ]
 })
 export class AppModule {}
